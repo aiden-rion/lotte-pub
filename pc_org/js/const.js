f3aec53bcb443ff44cf9cfae9209e2e52307b6b4
aiden-rion <153442767+aiden-rion@users.noreply.github.com>
2024-10-08 09:08:18 +0900

    PC 퍼블리싱 파일 등록

---
+++ pc_org/js/const.js
@@ -0,0 +1,15 @@
+//homeservice
+const CODE_HOME_PRESAVE0 = "00";
+const CODE_HOME_ORDER = "01";
+const CODE_HOME_READY = "02";
+const CODE_HOME_PAID = "03";
+const CODE_HOME_DELIVERY = "04";
+const CODE_HOME_BUY_COMPLETE = "05";
+const CODE_HOME_CANCEL_REQUEST = "10";
+const CODE_HOME_RETURN = "11";
+const CODE_HOME_RETURN_CHECK = "12";
+const CODE_HOME_RETURN_COMPLETE = "13";
+//lawm
+const CODE_LAWM_90 = "90";
+const CODE_LAWM_180 = "180";
+const CODE_LAWM_365 = "365";
---
+++ pc_org/js/ui-main-new.js
@@ -0,0 +1,581 @@
+/*** annotatorPro plugin ***/
+$("#the-img-tag").annotatorPro({
+	maxZoom : 2,
+	frameWidth : "100%",
+	frameHeight : "100%",
+	navigator : true,
+	navigatorImagePreview : true,
+	rubberband : true,
+	annotations : [
+		{
+			id : "tower0801",
+			title : "기아",
+			text : "K5",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "25%",
+			spot_top : "22%",
+			spot_width : "6%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0802",
+			title : "현대",
+			text : "아반떼 AD",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "35.1%",
+			spot_top : "21.7%",
+			spot_width : "3.9%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0701",
+			title : "벤츠",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "25.1%",
+			spot_top : "28.7%",
+			spot_width : "4.9%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0702",
+			title : "기아",
+			text : "레이",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "30.1%",
+			spot_top : "27.9%",
+			spot_width : "3.9%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0703",
+			title : "기아",
+			text : "올 뉴 모닝",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "35.5%",
+			spot_top : "29%",
+			spot_width : "3.6%",
+			spot_height : "3%",
+		},
+		{
+			id : "tower0704",
+			title : "현대",
+			text : "아반떼 AD",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "39.1%",
+			spot_top : "30.2%",
+			spot_width : "4.6%",
+			spot_height : "3.2%",
+		},
+		{
+			id : "tower0601",
+			title : "아우디",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "25.1%",
+			spot_top : "37.2%",
+			spot_width : "6%",
+			spot_height : "3%",
+		},
+		{
+			id : "tower0602",
+			title : "현대",
+			text : "그랜저IG",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "35.1%",
+			spot_top : "37%",
+			spot_width : "4%",
+			spot_height : "2.5%",
+		},
+		{
+			id : "towerHyudai",
+			title : "현대",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "33.4%",
+			spot_top : "39.8%",
+			spot_width : "2.6%",
+			spot_height : "3.7%",
+		},
+		{
+			id : "tower0501",
+			title : "현대",
+			text : "싼타페DM 더 프라임",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "25.1%",
+			spot_top : "44.2%",
+			spot_width : "4.9%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0502",
+			title : "기아",
+			text : "더 뉴 쏘렌토",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "30.1%",
+			spot_top : "44%",
+			spot_width : "4.2%",
+			spot_height : "3.4%",
+		},
+		{
+			id : "tower0503",
+			title : "기아",
+			text : "더 뉴 카니발",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "37.5%",
+			spot_top : "44.5%",
+			spot_width : "6.4%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0401",
+			title : "쌍용",
+			text : "티볼리",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "30.1%",
+			spot_top : "52%",
+			spot_width : "4%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0402",
+			title : "기아",
+			text : "레이",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "35.5%",
+			spot_top : "51.7%",
+			spot_width : "3.5%",
+			spot_height : "3.7%",
+		},
+		{
+			id : "tower0403",
+			title : "기아",
+			text : "올 뉴 모닝",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "39.1%",
+			spot_top : "52.2%",
+			spot_width : "4.6%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "towerKia",
+			title : "기아",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "33.3%",
+			spot_top : "55.7%",
+			spot_width : "2.6%",
+			spot_height : "4%",
+		},
+		{
+			id : "tower0301",
+			title : "아우디",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "25.1%",
+			spot_top : "59.9%",
+			spot_width : "4.9%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0302",
+			title : "현대",
+			text : "아반떼 AD",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "30.1%",
+			spot_top : "60%",
+			spot_width : "4.2%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0303",
+			title : "기아",
+			text : "K3",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "35.1%",
+			spot_top : "59.9%",
+			spot_width : "3.9%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0304",
+			title : "기아",
+			text : "스포티지",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "39.1%",
+			spot_top : "59.3%",
+			spot_width : "4.7%",
+			spot_height : "3.7%",
+		},
+		{
+			id : "tower0201",
+			title : "벤츠",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "25.1%",
+			spot_top : "67.6%",
+			spot_width : "4.9%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0202",
+			title : "현대",
+			text : "LF쏘나타 뉴라이즈",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "30.1%",
+			spot_top : "67.5%",
+			spot_width : "4.2%",
+			spot_height : "3.5%",
+		},
+		{
+			id : "tower0203",
+			title : "현대",
+			text : "그랜저IG",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "35.1%",
+			spot_top : "67.3%",
+			spot_width : "3.9%",
+			spot_height : "3.8%",
+		},
+		{
+			id : "tower0204",
+			title : "제네시스",
+			text : "G80",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "39.1%",
+			spot_top : "67.2%",
+			spot_width : "4.8%",
+			spot_height : "3.3%",
+		},
+		{
+			id : "tower0101",
+			title : "기아",
+			text : "스포티지",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "25.1%",
+			spot_top : "74%",
+			spot_width : "5%",
+			spot_height : "4.5%",
+		},
+		{
+			id : "tower0102",
+			title : "기아",
+			text : "더 뉴 카니발",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "30.1%",
+			spot_top : "73.7%",
+			spot_width : "4.4%",
+			spot_height : "5%",
+		},
+		{
+			id : "tower0103",
+			title : "현대",
+			text : "싼타페DM 더 프라임",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "35.1%",
+			spot_top : "73.9%",
+			spot_width : "3.9%",
+			spot_height : "4.7%",
+		},
+		{
+			id : "tower0104",
+			title : "기아",
+			text : "더 뉴 쏘렌토",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "39.1%",
+			spot_top : "73.7%",
+			spot_width : "4.8%",
+			spot_height : "4%",
+		},
+		{
+			id : "arcadeGenesis",
+			title : "제네시스",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "51%",
+			spot_top : "56.7%",
+			spot_width : "9.8%",
+			spot_height : "5.8%",
+		},
+		{
+			id : "arcadeBMW",
+			title : "BMW",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "62.1%",
+			spot_top : "56.5%",
+			spot_width : "9.8%",
+			spot_height : "5.6%",
+		},
+		{
+			id : "arcadeSpecial",
+			title : "테마기획전",
+			text : "",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "52.1%",
+			spot_top : "62.9%",
+			spot_width : "8.8%",
+			spot_height : "7%",
+		},
+		{
+			id : "arcadeAudi",
+			title : "아우디",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "62.1%",
+			spot_top : "62.7%",
+			spot_width : "9.8%",
+			spot_height : "6%",
+		},
+		{
+			id : "arcadeSedan",
+			title : "대형 승용",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "51.1%",
+			spot_top : "72.3%",
+			spot_width : "4.4%",
+			spot_height : "4%",
+		},
+		{
+			id : "arcadeSUV",
+			title : "SUV",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "56.1%",
+			spot_top : "71.7%",
+			spot_width : "4.5%",
+			spot_height : "4.4%",
+		},
+		{
+			id : "arcadeBenz",
+			title : "벤츠",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "62.5%",
+			spot_top : "70.8%",
+			spot_width : "11%",
+			spot_height : "4.8%",
+		},
+		{
+			id : "arcadeSellcar",
+			title : "내차팔기",
+			text : "",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "56.5%",
+			spot_top : "76.9%",
+			spot_width : "7.8%",
+			spot_height : "6%",
+		},
+		{
+			id : "arcadeElectric",
+			title : "전기차",
+			text : "전체차량",
+			tint_color : "#fff",
+			popup_width : "122",
+			popup_height : "60",
+			spot_left : "65%",
+			spot_top : "75.7%",
+			spot_width : "10.8%",
+			spot_height : "6%",
+		},
+	]
+});
+
+/*** custom event ***/
+function initHomeUiEvent(){
+
+	// $(".ndd-annotator-content, .ndd-annotator-navigator-window").css("top", "0px");
+
+	//car click -> list open
+	$(document).on('click', '.ndd-spot',function(e) {
+		nayuda.showHomeCarListPopup(e.currentTarget.id);
+		$(this).trigger("mouseleave");
+	});
+
+	//car mouseenter -> image change
+	$(document).on('mouseenter', '.ndd-spot',function(e) {
+
+		$(".ndd-annotator-content").addClass("motion--hover");
+		$(this).addClass("motion--hover");
+
+	});
+
+	//car mouseleave -> image change
+	$(document).on('mouseleave', '.ndd-spot',function(e) {
+
+		$(".ndd-annotator-content").removeClass("motion--hover");
+		$(this).removeClass("motion--hover");
+		
+	});
+
+	//info layer control
+	$("#btMarketInfo").on("click", function(){
+		$('#layerMarketInfo').toggle();
+		$(this).toggleClass('on');
+		$('#layerNaviInfo').hide();
+		$("#btNaviInfo").removeClass("on");
+	})
+	$("#btNaviInfo").on("click", function(){
+		$('#layerNaviInfo').toggle();
+		$(this).toggleClass('on');
+		$('#layerMarketInfo').hide();
+		$("#btMarketInfo").removeClass("on");
+	})
+	$("#layerNaviInfo .bt-close-main-layer").on("click", function(){
+		$('#layerNaviInfo').toggle();
+		$('.btn_naviinfo').toggleClass('on');
+		$("body").removeClass("modalOpen");
+
+		//cookie make
+		if($("#todayHidden").is(":checked")){
+			nayuda.setCookie("mainNaviTodayHidden","true",1);
+		}
+	})
+	$("#todayHidden").on("click", function(){
+		$('#layerNaviInfo').toggle();
+		$('.btn_naviinfo').toggleClass('on');
+		//cookie make
+		nayuda.setCookie("mainNaviTodayHidden","true",1);
+	})
+	$("#layerMarketInfo .bt-close-main-layer").on("click", function(){
+		$('#layerMarketInfo').toggle();
+		$('.btn_marketinfo').toggleClass('on');
+		$("body").removeClass("modalOpen");
+
+	})
+	/*
+	$(window).on({
+		scroll : function(){
+			if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+				$("#mainInfoBtns").addClass("relate");
+			}else{
+				$("#mainInfoBtns").removeClass("relate");
+			}
+			
+		},
+		resize : function(){
+
+			if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+				$("#mainInfoBtns").addClass("relate");
+			}else{
+				$("#mainInfoBtns").removeClass("relate");
+			}
+
+		}
+	}).trigger('scroll');
+	*/
+	$("header .gnb .nav-group:not(.group-util)  .mn").on("mouseenter", function(){
+		var depth1 = $(this).parents(".depth1");
+
+		$("header .gnb .depth2").hide();
+		depth1.find(".depth2").show();
+		$(".layout--header").addClass("gnbOn");
+	})
+
+	$("header .gnb").on("mouseleave", function(){
+		$("header .gnb .depth2").hide();
+		$(".layout--header").removeClass("gnbOn");
+	})
+
+	var naviWidth = $(".ndd-annotator-navigator").innerWidth();
+	var naviHeight = $(".ndd-annotator-navigator").innerHeight();
+
+	$( document ).ready( function() {
+		//cookie check on load
+		if(nayuda.getCookie("mainNaviTodayHidden") != "true") {
+			$("#layerNaviInfo").css( "display", "block");
+		}
+
+		$(".main-nav-box").css( "width", naviWidth*0.8);
+		$(".main-nav-box").css( "top", naviHeight*0.8+160);
+		$(".btns").css( "width", naviWidth*0.8);
+		$(".main-nav-box").css( "display", "block");
+	})
+
+	//layout popup close on press ESC
+	$(document).keydown(function(e){
+		var code = e.keyCode || e.which;
+
+		if (code == 27) {
+			$("#layerNaviInfo").css("display", "none");
+
+			//cookie make
+			if($("#todayHidden").is(":checked")){
+				nayuda.setCookie("mainNaviTodayHidden","true",1);
+			}
+		}
+	});
+}
